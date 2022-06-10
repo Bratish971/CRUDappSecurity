adecc142a3ef50338097e8798abae8820a6272da
Bratish971 <90181179+Bratish971@users.noreply.github.com>
2022-06-10 12:35:34 +0300

    RoleService Transactional refactor

--- src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/RoleServiceImpl.java
@@ -7,25 +7,23 @@ import ru.kata.spring.boot_security.demo.dao.RoleRepository;
 import ru.kata.spring.boot_security.demo.model.Role;
 
 @Service
+@Transactional
 public class RoleServiceImpl implements RoleService{
 
     @Autowired
     private RoleRepository roleRepository;
 
     @Override
-    @Transactional
     public Iterable<Role> findAll() {
         return roleRepository.findAll();
     }
 
     @Override
-    @Transactional
     public Role findById(long id) {
         return roleRepository.findById(id).get();
     }
 
     @Override
-    @Transactional
     public void save(Role role) {
         roleRepository.save(role);
     }
