a6696b3e2d0193283526f6f636ac9007f2081e51
Bratish971 <90181179+Bratish971@users.noreply.github.com>
2022-06-10 12:30:00 +0300

    Service Transactional refactor

--- src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -9,37 +9,33 @@ import ru.kata.spring.boot_security.demo.model.User;
 import java.util.List;
 
 @Service
+@Transactional
 public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserRepository userRepository;
 
     @Override
-    @Transactional
     public User findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
     @Override
-    @Transactional
     public Iterable<User> findAll() {
         return userRepository.findAll();
     }
 
     @Override
-    @Transactional
     public User findById(long id) {
         return userRepository.findById(id).get();
     }
 
     @Override
-    @Transactional
     public void save(User user) {
         userRepository.save(user);
     }
 
     @Override
-    @Transactional
     public void delete(User byId) {
         userRepository.delete(byId);
     }
